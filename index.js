99fe2c7413675aea67ad53c183b19fb1ea2d3243
y3k0000 <y3k00000@github.com>
2020-03-10 15:09:56 +0800

    new magnet

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const WebTorrent = require('webtorrent');
 
 console.log(WebTorrent.WEBRTC_SUPPORT);
 
-const magnet = "magnet:?xt=urn:btih:0a732ea77424fec27fd98201d97a820bbf64e0f0&dn=ic_launcher.png&tr=ws%3A%2F%2Fapi.cmoremap.com.tw%3A5566&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com";
+// const magnet = "magnet:?xt=urn:btih:0a732ea77424fec27fd98201d97a820bbf64e0f0&dn=ic_launcher.png&tr=ws%3A%2F%2Fapi.cmoremap.com.tw%3A5566&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com";
+const magnet = "magnet:?xt=urn:btih:0a732ea77424fec27fd98201d97a820bbf64e0f0&dn=ic_launcher.png&tr=ws%3A%2F%2Fapi.cmoremap.com.tw%3A5566";
 let wtClient = new WebTorrent();
 
 wtClient.add(magnet, async torrent=>{
